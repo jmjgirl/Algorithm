02a1daad643bd3327ba7928ecb42d2ecf71e2b44
Minji Chung <jmjcap123@gmail.com>
2023-08-05 15:04:36 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,6 +1,12 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
+        
+        for(int i = 0; i < num_list.length; i++) {
+            answer[num_list[i] % 2]++;
+        }
+            
+        /* 첫번째 풀었던 방법
         for(int i=0; i < num_list.length; i++) {
             if (num_list[i] % 2 == 0) {
                 answer[0] += 1;
@@ -8,6 +14,7 @@ class Solution {
                 answer[1] += 1;
             }
         }
+        */
         return answer;
     }
 }
