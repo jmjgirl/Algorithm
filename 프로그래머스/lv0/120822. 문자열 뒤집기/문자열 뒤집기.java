58120d1a06ac5738a7eb1be75ba396d1eef5462b
Minji Chung <jmjcap123@gmail.com>
2023-08-11 14:54:48 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,5 +1,8 @@
 class Solution {
     public String solution(String my_string) {
+        
+        return new StringBuilder(my_string).reverse().toString();
+        /*
         String answer = "";
         
         for (int i = my_string.length()-1; i >= 0; i--) {
@@ -7,5 +10,6 @@ class Solution {
         }
         
         return answer;
+        */
     }
 }
