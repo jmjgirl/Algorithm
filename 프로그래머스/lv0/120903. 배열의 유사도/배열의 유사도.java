bd6177066ec783ee1990bc2f052c38c45b0b7b90
Minji Chung <jmjcap123@gmail.com>
2023-08-11 14:39:51 +0900

    [level 0] Title: 배열의 유사도, Time: 0.08 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        
+        List<String> list = Arrays.asList(s2);
+        for (int i = 0; i < s1.length; i++) {
+            answer += list.contains(s1[i]) ? 1 : 0;
+        }
+        return answer;
+    }
+}
