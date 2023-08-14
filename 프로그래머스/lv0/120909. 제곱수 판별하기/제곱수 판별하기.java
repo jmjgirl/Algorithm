e4a30cd180fc4f059d42c0107e6ecdc95267bf80
Minji Chung <jmjcap123@gmail.com>
2023-08-14 11:02:46 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,10 @@
+import java.math.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        answer = Math.sqrt(n) % 1  == 0 ? 1 : 2;
+        return answer;
+    }
+}
