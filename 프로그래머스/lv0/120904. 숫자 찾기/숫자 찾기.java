7d1bc73cffaac84022b49029752160389637eda1
Minji Chung <jmjcap123@gmail.com>
2023-08-18 11:19:54 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,16 +1,13 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
-        String [] number = String.valueOf(num).split("");
+        int answer = -1;
+        String number = String.valueOf(num);
         
-        for(int i = 0; i < number.length; i++) {
-            if(number[i].equals(String.valueOf(k))) {
-                answer = i+1;
-                break;
+        answer = number.indexOf(String.valueOf(k));
+        if(answer >= 0) {
+                return answer + 1;
             } else {
-                answer = -1;
+                return -1;
             }
         }
-        return answer;
-    }
 }
