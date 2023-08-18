414170062f350790f4ead34de5d44a8770b59ee9
Minji Chung <jmjcap123@gmail.com>
2023-08-18 11:50:10 +0900

    [level 0] Title: 중복된 문자 제거, Time: 2.06 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,16 +1,9 @@
 import java.util.*;
-import static java.util.stream.Collectors.*;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
         
-        String [] arr = my_string.split("");
-        
-        String[] resultArr = Arrays.stream(arr).distinct().toArray(String[]::new);
-        for(String word : resultArr) {
-            answer += word;
-        }
-        return answer;
+        return Arrays.stream(my_string.split("")).distinct().collect(Collectors.joining());
     }
 }
