e5efa949177ee71d2e580e7df7cf5fdf1c0d6dc7
Minji Chung <jmjcap123@gmail.com>
2023-08-18 11:30:03 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.44 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -2,6 +2,14 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
+        
+        char[] str = my_string.toLowerCase().toCharArray();
+
+        Arrays.sort(str);
+
+        String answer = new String(str);
+        
+        /*
         String answer = "";
         my_string = my_string.toLowerCase();
         
@@ -12,7 +20,7 @@ class Solution {
         for(String num : arr){
             answer += num;
         }
-        
+        */
         return answer;
     }
 }
