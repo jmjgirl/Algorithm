66f215336bc207e6ff83b3d3fca40b37e11b79fd
Minji Chung <jmjcap123@gmail.com>
2023-08-21 10:42:42 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
         int multiple = 1;
+        
         for (int i = 1; i <= n; i++) {
             multiple *= i;
             if (multiple == n) {
