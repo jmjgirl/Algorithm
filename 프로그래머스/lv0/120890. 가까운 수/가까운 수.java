c0d24e8c8cebf1f8dedde60f04c46429e1c8765b
Minji Chung <jmjcap123@gmail.com>
2023-08-21 11:25:16 +0900

    [level 0] Title: 가까운 수, Time: 0.44 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -4,11 +4,14 @@ import java.util.*;
 class Solution {
     public int solution(int[] array, int n) {
 
+        // 1. 정렬되었다는 말이 없으니까 먼저 정렬
         Arrays.sort(array);
+        
         int near = Math.abs(n-array[0]);
         int answer = array[0];
         
         for (int i = 1; i < array.length; i++) {
+            // 가장 가까운 수가 여러개 일 경우 더 작은 수
             if (near == Math.abs(n-array[i])) {
                 answer = array[i-1];
             } else if (near >= Math.abs(n-array[i])) {
