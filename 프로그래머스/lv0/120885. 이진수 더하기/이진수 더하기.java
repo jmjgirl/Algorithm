a3d89e37e3eead3b23afbf3da22f55f6c5ea4533
Minji Chung <jmjcap123@gmail.com>
2023-08-21 14:15:19 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
@@ -2,6 +2,8 @@ class Solution {
     public String solution(String bin1, String bin2) {
         String answer = "";
         
+        // 10진수 -> 2진수 : Integer.toBinaryString()
+        // 2진수 -> 10진수 : Integer.parseInt(String s, int n진수)
         answer = Integer.toBinaryString(Integer.parseInt(bin1,2) + Integer.parseInt(bin2,2));
         return answer;
     }
