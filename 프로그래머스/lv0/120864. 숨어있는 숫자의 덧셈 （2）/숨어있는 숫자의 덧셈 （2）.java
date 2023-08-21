2b2d3931e46200019cb5256efd0c9c33f7db5206
Minji Chung <jmjcap123@gmail.com>
2023-08-21 14:02:07 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.37 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String [] str = my_string.split("[a-zA-Z]");
+
+        for (String s : str) {
+            if (s.matches("[0-9]+")) {
+                answer += Integer.parseInt(s);
+            }
+        }
+        return answer;
+    }
+}
