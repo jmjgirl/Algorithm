0f9846909f92bd66cf180b25401dcb5cce5c2d4a
Minji Chung <jmjcap123@gmail.com>
2023-08-21 14:04:22 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.38 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -4,6 +4,7 @@ class Solution {
         String [] str = my_string.split("[a-zA-Z]");
 
         for (String s : str) {
+            // [0-9]+ : 0-9까지 최소 한번 이상 반복
             if (s.matches("[0-9]+")) {
                 answer += Integer.parseInt(s);
             }
