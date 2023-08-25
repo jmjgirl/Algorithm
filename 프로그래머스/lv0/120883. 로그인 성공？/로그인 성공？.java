1fcae87de7b15a869cc70081565995e594c6d433
Minji Chung <jmjcap123@gmail.com>
2023-08-25 11:16:09 +0900

    [level 0] Title: 로그인 성공?, Time: 0.01 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -3,12 +3,15 @@ class Solution {
         String answer = "";
         
         for (int i = 0; i < db.length; i++) {
-            if (db[i][0].equals(id_pw[0]) && db[i][1].equals(id_pw[1])) {
-                answer = "login";
-                break;
-            } else if (db[i][0].equals(id_pw[0])) {
-                answer = "wrong pw";
-                break;
+            if (db[i][0].equals(id_pw[0])) {
+                if (db[i][1].equals(id_pw[1])) {
+                    answer = "login";
+                    break;
+                } else {
+                    answer = "wrong pw";
+                    break;
+                }
+                
             } else {
                 answer = "fail";
             }
