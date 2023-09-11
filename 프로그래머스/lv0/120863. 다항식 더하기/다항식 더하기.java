0660ba7ecc1bc9f1f8aa0c2a8b095c84562c8d96
Minji Chung <jmjcap123@gmail.com>
2023-09-11 16:26:47 +0900

    [level 0] Title: 다항식 더하기, Time: 7.08 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String polynomial) {
+        String answer = "";
+        int x = 0; // ~x값
+        int n = 0; // 정수
+        
+        String [] arr = polynomial.split(" ");
+        
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i].contains("x")) {
+                x += arr[i].equals("x") ? 1 : Integer.parseInt(arr[i].replace("x",""));
+                //System.out.print(Integer.parseInt(arr[i].replace("x","")));
+            } else if(!arr[i].equals("+")) {
+                n += Integer.parseInt(arr[i]);
+            }
+        }
+        
+        if (x==1 && n>=1) {
+            answer = "x + " + n;
+        } else if (x>=1 && n>=1) {
+            answer = x==1 ? "x + " + n : x + "x + " + n;
+        } else if (x==0 && n>=1) {
+            answer = "" + n;
+        } else if (x>=1 && n==0) {
+            answer = x==1 ? "x" : x + "x";
+        } 
+
+        return answer;
+    }
+}
