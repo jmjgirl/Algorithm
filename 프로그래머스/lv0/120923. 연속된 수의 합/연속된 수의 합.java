3e4af4a15ef760e2571fc2390ddc75ce03dc0f78
Minji Chung <jmjcap123@gmail.com>
2023-09-12 11:30:35 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -2,6 +2,17 @@ class Solution {
     public int[] solution(int num, int total) {
         int[] answer = new int[num];
         
+        int temp = 0;
+        for(int i=0;i<num;i++){
+            temp+=i;
+        }
+        
+        int value = (total-temp)/num;
+
+        for(int i=0;i<num;i++){
+            answer[i]=i+value;
+        }
+        /*
         if (total % num == 0) {
             for (int i=0; i<num/2; i++) {
                 answer[i] = (total/num) - (num/2-i);
@@ -19,6 +30,8 @@ class Solution {
                 answer[j] = answer[j-1] +1;
             }
         }
+        */
+        
         return answer;
     }
 }
