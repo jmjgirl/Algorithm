2ec053d79f2832c04c5d58fb9717864a2a386add
Minji Chung <jmjcap123@gmail.com>
2023-09-12 13:06:31 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -5,22 +5,6 @@ class Solution {
         int denom = denom1 * denom2;
         int numer = numer1 * (denom / denom1) + numer2 * (denom / denom2);
         
-        
-        /*
-        int denom = 0;
-        // 분모
-        if (denom2 % denom1 == 0) {
-            denom = denom2;
-        } else if (denom1 % denom2 == 0) {
-            denom = denom1;
-        } else {
-            denom = (denom2 / (denom2 % denom1)) * denom1;
-        }
-        
-        // 분자
-        int numer = (numer1 * (denom / denom1) + numer2 * (denom / denom2));
-        */
-
         answer[0] = numer / GCD(numer,denom);
         answer[1] = denom / GCD(numer,denom);
         
