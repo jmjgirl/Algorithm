0307d6c749d9062af378b504f15fcfc3d6e545d5
Minji Chung <jmjcap123@gmail.com>
2023-09-12 10:50:34 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+        // 등차수열일때
+        if((common[1] - common[0]) == (common[2] - common[1])) {
+            answer = common[common.length-1] + (common[1] - common[0]);
+            // 등비수열일때
+        } else {
+            answer = common[common.length-1] * (common[1] / common[0]);
+        }
+        
+        return answer;
+    }
+}
