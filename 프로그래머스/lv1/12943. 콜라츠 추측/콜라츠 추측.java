c684a58521bd13f91c6818e90ac543f277e3cbec
Minji Chung <jmjcap123@gmail.com>
2023-09-19 10:57:43 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        
+        if(num == 1) {
+            return 0;
+        }
+        
+        while(num != 1) {
+            num = num % 2 == 0 ? num/2 : num*3 + 1;
+            answer++;
+            if (answer == 500) {
+                return -1;
+            }
+        }
+        return answer;
+    }
+}
