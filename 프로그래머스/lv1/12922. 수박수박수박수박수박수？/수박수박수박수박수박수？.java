554fe72e383e78dc0c40b5fdbf9f34d426b53fa8
Minji Chung <jmjcap123@gmail.com>
2023-09-21 11:26:47 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.73 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,6 +1,7 @@
 class Solution {
     public String solution(int n) {
         String answer = "";
+        
         if(n % 2 == 0) answer = "수박".repeat(n/2);
         else answer = "수박".repeat(n/2) + "수";
 
