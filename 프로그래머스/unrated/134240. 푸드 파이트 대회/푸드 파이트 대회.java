e04fac7c18887125c94c13ee467d0e2d1591369e
Minji Chung <jmjcap123@gmail.com>
2023-09-27 11:55:31 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 10.55 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -6,7 +6,6 @@ class Solution {
         for(int i=0; i<food.length; i++) {
             count += food[i]%2==0 ? food[i] : food[i]-1;
         }
-        //System.out.println(count);
         
         int[] arr = new int[count];
         int left = 0; 
@@ -14,7 +13,7 @@ class Solution {
         
         for(int i=0; i<food.length; i++) {
             int repeat = food[i] / 2;
-            //System.out.println(repeat);
+ 
             if(repeat > 0) {
                 for(int j=0; j<repeat; j++) {
                     System.out.println(i);
