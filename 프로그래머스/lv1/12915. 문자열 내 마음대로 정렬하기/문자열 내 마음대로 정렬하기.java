e6f8ad34e8ae7f470e50f750808af6b1d911a27d
Minji Chung <jmjcap123@gmail.com>
2023-09-27 13:23:17 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.56 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -3,14 +3,15 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] strings, int n) {
         String[] answer = new String[strings.length];
-        int[] dict = new int[strings.length]; // 순서 저장
+        int[] dict = new int[strings.length]; // 순서 저장 (0이 사전순 제일 앞쪽)
         
         for(int i=0; i<strings.length; i++) {
             for(int j=0; j<strings.length; j++) {
-                // i!=j이고 
-                if(strings[i].charAt(n) > strings[j].charAt(n)) dict[i] +=1;
-                else if(strings[i].charAt(n) == strings[j].charAt(n)) {
-                    if(strings[i].compareTo(strings[j]) > 0) {
+                
+                if(strings[i].charAt(n) > strings[j].charAt(n)) {
+                    dict[i] +=1;
+                } else if(strings[i].charAt(n) == strings[j].charAt(n)) {
+                    if(strings[i].compareTo(strings[j]) > 0) { // i가 j보다 사전순 뒤에 있으면
                         dict[i]+=1;
                     }
                 }
