989236e6464562a187478e5c200c1881a3215da9
Minji Chung <jmjcap123@gmail.com>
2023-10-10 14:07:38 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(int a, int b) {
-        String answer = "";
         String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
         int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};        
 
