42bcd23af9bd40022b87718522a10b6e87ee6984
Minji Chung <jmjcap123@gmail.com>
2023-10-10 15:53:26 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.32 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -18,23 +18,6 @@ class Solution {
             answer[i] = priorityQueue.peek();
         }
         
-        /*
-        LinkedList<Integer> ll = new LinkedList();
-        
-        for(int i=0; i<score.length; i++) {
-            
-            if(ll.size() >= k && ll.get(0) < score[i]) {
-                ll.remove(0);
-                ll.add(score[i]);
-            } else if (ll.size() < k) {
-                ll.add(score[i]);
-            }
-            
-            Collections.sort(ll);
-            answer[i] = ll.get(0);
-        }
-        */
-        
         return answer;
     }
 }
