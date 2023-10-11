324903b035764f196c3e8493cc0c28bbdacde9cb
Minji Chung <jmjcap123@gmail.com>
2023-10-11 11:36:30 +0900

    [level 1] Title: 소수 찾기, Time: 12.46 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -3,17 +3,19 @@ import java.math.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        boolean flag = true;
-        for(int i=2; i<=n; i++) {
-            for(int j=2; j<=Math.sqrt(i); j++) {
-                if(i%j==0) {
-                    flag = false;
-                    break;
+        boolean[] number = new boolean[n+1];
+        // 소수가 아니면 true
+        
+        for(int i=2; i <= n; i++) {
+            if(number[i] != true) {
+                for(int j=i*2; j<= n; j+=i) {
+                    number[j] = true;
                 }
             }
-            
-            if(flag==true) answer++;
-            flag = true;
+        }
+        
+        for(int i=2; i<=n; i++) {
+            if(number[i] == false) answer++;
         }
         return answer;
     }
