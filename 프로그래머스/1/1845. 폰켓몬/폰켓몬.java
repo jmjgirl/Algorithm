3e0538717abe0fcc986e5130684f702d38dd1b96
Minji Chung <jmjcap123@gmail.com>
2023-10-11 14:51:54 +0900

    [level 1] Title: 폰켓몬, Time: 6.03 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -7,8 +7,7 @@ class Solution {
         // N/2
         int[] result = Arrays.stream(nums).distinct().toArray();
         //System.out.println(result.length);
-        if(result.length <= nums.length / 2) answer = result.length;
-        else answer = nums.length/2;
+        answer = result.length <= nums.length / 2 ? result.length : nums.length/2;
         return answer;
     }
 }
