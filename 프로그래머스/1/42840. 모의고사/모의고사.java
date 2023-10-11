ee5ca0ef98457b9831d714a28ba4d819708ec12d
Minji Chung <jmjcap123@gmail.com>
2023-10-11 14:05:10 +0900

    [level 1] Title: 모의고사, Time: 1.07 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] num1 = {1,2,3,4,5};
+        int[] num2 = {2,1,2,3,2,4,2,5};
+        int[] num3 = {3,3,1,1,2,2,4,4,5,5};
+        
+        int[] score = new int[3];
+        
+        for(int i=0; i<answers.length; i++) {
+            if(answers[i] == num1[i%5]) score[0]++;
+        }
+        
+        for(int i=0; i<answers.length; i++) {
+            if(answers[i] == num2[i%8]) score[1]++;
+        }
+        
+        for(int i=0; i<answers.length; i++) {
+            if(answers[i] == num3[i%10]) score[2]++;
+        }
+        
+        int max = score[0];
+        for(int i=1; i<3; i++) {
+            if(max < score[i]) max = score[i];
+        }
+        
+        int count = 0;
+        for(int i=0; i<3; i++) {
+            if(score[i] == max) count++;
+        }
+        
+        int[] answer = new int[count];
+        int index = 0;
+        for(int i=0; i<3; i++) {
+            if(score[i] == max) answer[index++] = i+1;
+            
+        }
+        
+        
+        
+        return answer;
+    }
+}
