85b7d6a2b481bbd466342827941c67552f021a7b
Minji Chung <jmjcap123@gmail.com>
2023-10-17 11:09:48 +0900

    [level 1] Title: 옹알이 (2), Time: 5.96 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,19 +1,11 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] baby = {"aya","ye","woo","ma"};
         
         for(int i=0; i<babbling.length; i++) {
             babbling[i] = babbling[i].replaceAll("ayaaya|yeye|woowoo|mama", " ");
             babbling[i] = babbling[i].replaceAll("aya|ye|woo|ma", "");
-            /*
-            for(int j=0; j<baby.length; j++) {
-                babbling[i] = babbling[i].replace(baby[j],"");
-            }  
-            */
-            //System.out.println(babbling[i]);
         }
-        
         
         for(int i=0; i<babbling.length; i++) {
             if(babbling[i].length() == 0) answer++;
