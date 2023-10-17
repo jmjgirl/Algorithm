34a9ac1437b63c7e0beacbd0e023b74054b013d5
Minji Chung <jmjcap123@gmail.com>
2023-10-17 10:36:51 +0900

    [level 1] Title: 실패율, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,6 +2,39 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
+        
+        int[] answer = new int[N];
+        double[] tempArr = new double[N];
+        int arrLength = stages.length;
+        int idx = arrLength;
+        double tempD = 0;
+        int tempI = 0;
+        for (int i = 0; i < arrLength; i++) {
+            int stage = stages[i];
+            if (stage != N + 1)
+                answer[stage - 1] += 1;
+        }
+        for (int i = 0; i < N; i++) {
+            int personNum = answer[i];
+            tempArr[i] = (double) personNum / idx;
+            idx -= personNum;
+            answer[i] = i + 1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j < N - i; j++) {
+                if (tempArr[j - 1] < tempArr[j]) {
+                    tempD = tempArr[j - 1];
+                    tempArr[j - 1] = tempArr[j];
+                    tempArr[j] = tempD;
+
+                    tempI = answer[j - 1];
+                    answer[j - 1] = answer[j];
+                    answer[j] = tempI;
+                }
+            }
+        }
+        /*
         int[] answer = new int[N];
         
         double[] pass = new double[N];
@@ -36,6 +69,7 @@ class Solution {
             answer[i] = index;
             hashmap.remove(index);
         }
+        */
         
         return answer;
     }
