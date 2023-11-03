4c1c994b8ae6709ee3fee903582dd8376713b296
Minji Chung <jmjcap123@gmail.com>
2023-11-03 09:25:44 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.40 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -3,8 +3,6 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         String[] str = s.split(" ");
-        
-        /*
         int[] istr = new int[str.length];
         
         for(int i=0; i<str.length; i++) {
@@ -12,18 +10,8 @@ class Solution {
         }
         
         Arrays.sort(istr);
-        answer += istr[0] + " " + istr[istr.length-1];
-        */
-        
-        int max=Integer.parseInt(str[0]);
-        int min=Integer.parseInt(str[0]);
-        for(int i=1; i<str.length; i++) {
-            int n=Integer.parseInt(str[i]);
-            if(min > n) min=n;
-            if(n > max) max=n;
-        }
-        return min + " " + max;
-        
-        //return answer;
+        answer = istr[0] + " " + istr[istr.length-1];
+    
+        return answer;
     }
 }
