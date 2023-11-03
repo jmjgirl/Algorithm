4f023da4988a8919c650dcf4da3a9296c168a674
Minji Chung <jmjcap123@gmail.com>
2023-11-03 16:30:14 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.01 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -4,23 +4,21 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         String[] arr = s.split(" ");
-        System.out.println(Arrays.toString(arr));
+        
         for(int i = 0; i < arr.length; i++) {
             
-            if(arr[i].length() == 0) answer += " "; // 단어가 공백일 경우 반환값에 공백 더하기
+            if(arr[i].length() == 0) answer += " "; 
             
-            else{
-                answer += arr[i].substring(0, 1).toUpperCase(); // 단어 첫번째 대문자로 변환하여 더하기
-                answer += arr[i].substring(1, arr[i].length()).toLowerCase(); // 첫글자를 제외하고 소문자로 변환하여 더하기
-                answer += " "; // 띄어쓰기
+            else {
+                answer += arr[i].substring(0, 1).toUpperCase(); 
+                answer += arr[i].substring(1, arr[i].length()).toLowerCase(); 
+                answer += " "; 
             }
             
         }
         
-        //원래 문자열 마지막이 공백일 경우 그대로 answer 반환
         if(s.substring(s.length() -1, s.length()).equals(" ")) return answer;
         
-        //마지막에 공백이 더해져서 그 공백을 제외한 answer값 반환
         return answer.substring(0, answer.length() - 1);
         
     }
