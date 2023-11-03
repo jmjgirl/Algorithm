800afc78cefec574395e390ab2b65238a0cc200d
Minji Chung <jmjcap123@gmail.com>
2023-11-03 16:27:11 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.31 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -4,28 +4,7 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         String[] arr = s.split(" ");
-        
-        /*
-        for(int i=0; i<arr.length; i++) {
-            arr[i] = arr[i].toLowerCase();
-
-            for(int j=0; j<arr[i].length(); j++) {
-                if(arr[i].charAt(j) >= 'a' && arr[i].charAt(j) <= 'z') {
-                    arr[i] = Character.toUpperCase(arr[i].charAt(j)) + arr[i].substring(j+1);
-                    break;
-                } else if(arr[i].charAt(j) >= '0' && arr[i].charAt(j) <= '9') {
-                    arr[i] = arr[i];
-                    break;
-                } 
-            }
-        }
-        
-        answer = String.join(" ", arr);
-        
-        return answer;
-        */
-        
-        // 공백을 기준으로 잘려진 문자열의 수만큼 반복
+        System.out.println(Arrays.toString(arr));
         for(int i = 0; i < arr.length; i++) {
             
             if(arr[i].length() == 0) answer += " "; // 단어가 공백일 경우 반환값에 공백 더하기
