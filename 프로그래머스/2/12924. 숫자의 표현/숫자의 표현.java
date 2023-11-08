96a8333acea84f66a3f3d3a716570f9d085372d4
Minji Chung <jmjcap123@gmail.com>
2023-11-08 09:00:30 +0900

    [level 2] Title: 숫자의 표현, Time: 0.90 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        int answer = 1;
         int sum = 0;
         
-        for(int i=1; i<=n; i++) {
+        for(int i=1; i<=n/2; i++) {
             int num = i;
-            while(sum < n && num <= n) {
+            while(sum < n) {
                 sum += num;
                 num++;
                 if(sum == n) answer++;
