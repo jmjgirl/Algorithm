c1ed959653fb1bf43b5f509a0be886bb8e6c3ccd
Minji Chung <jmjcap123@gmail.com>
2023-11-08 10:46:03 +0900

    [level 2] Title: 짝지어 제거하기, Time: 46.29 ms, Memory: 59 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -8,13 +8,10 @@ class Solution
         Stack<Character> st = new Stack<>();
         
         for(int i=0; i<s.length(); i++) {
-            if(st.isEmpty()){
-                st.push(s.charAt(i));
+            if(!st.isEmpty() && st.peek() == s.charAt(i)){
+                st.pop();
             } else {
-                if(st.peek() == s.charAt(i))
-                    st.pop();
-                else
-                    st.push(s.charAt(i));
+                st.push(s.charAt(i));
             }
         }
     
