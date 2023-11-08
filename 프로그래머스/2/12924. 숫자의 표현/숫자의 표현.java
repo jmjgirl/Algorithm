4c3b1627f72ae144aca7b9ba13023ce46e853072
Minji Chung <jmjcap123@gmail.com>
2023-11-08 09:03:06 +0900

    [level 2] Title: 숫자의 표현, Time: 0.86 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -3,12 +3,15 @@ class Solution {
         int answer = 1;
         int sum = 0;
         
+        // n/2을 넘어서서 연속된 숫자를 더해봤자 필요가 없음
         for(int i=1; i<=n/2; i++) {
             int num = i;
             while(sum < n) {
-                sum += num;
-                num++;
-                if(sum == n) answer++;
+                sum += num++;
+                if(sum == n) {
+                    answer++;
+                    break;
+                }
             }
             
             sum = 0;
