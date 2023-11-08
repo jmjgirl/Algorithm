db06f1d7b2c13f7d41508df786ec917856767b00
Minji Chung <jmjcap123@gmail.com>
2023-11-08 09:25:12 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int n) {
+        int answer = n+1;
+        int count1 = 0;
+        
+        char[] arr1 = Integer.toBinaryString(n).toCharArray();
+        for(char c : arr1) {
+            if(c == '1') count1++;
+        }
+        
+        int count2 = 0;
+        while(answer > n) {
+            char[] arr2 = Integer.toBinaryString(answer).toCharArray();
+            for(char c : arr2) {
+                if(c == '1') count2++;
+            }
+            
+            if(count1 == count2) {
+                break;
+            } else {
+                count2 = 0;
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
