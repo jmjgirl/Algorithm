f4b19f49ef4062ac76eeda7b9eef81c7d8ddc2fe
Minji Chung <jmjcap123@gmail.com>
2023-11-14 14:09:19 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -6,8 +6,8 @@ class Solution
         int answer = 0;
 
         while(true) {
-            a = (a+1) / 2;
-            b = (b+1) / 2;
+            a = (a / 2) + (a % 2);
+            b = (b / 2) + (b % 2);
             answer++;
             
             if(a==b) break;
