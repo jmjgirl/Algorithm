d5c1d493331735af1f0d9f493b900cf5d929a2de
Minji Chung <jmjcap123@gmail.com>
2023-11-18 13:14:40 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -9,13 +9,10 @@ class Solution {
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0) + 1);
         }
         
-        Set set = map.entrySet();
-        Iterator it = set.iterator();
-        
-        while(it.hasNext()) {
-            Map.Entry e = (Map.Entry) it.next();
-            System.out.println(e.getValue());
-            answer *= (int)e.getValue()+1;
+        Iterator<String> keys = map.keySet().iterator(); 
+        while( keys.hasNext() ){ 
+            String key = keys.next(); 
+            answer *= map.get(key)+1;
         }
         
         answer -= 1;
