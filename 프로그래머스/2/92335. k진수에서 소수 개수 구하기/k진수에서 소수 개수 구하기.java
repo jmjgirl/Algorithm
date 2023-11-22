03d9f70148f34222e07bb09c5b103e317e43aada
Minji Chung <jmjcap123@gmail.com>
2023-11-22 12:20:11 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.31 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.math.*;
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        String str = Integer.toString(n,k);
+        String[] arr = str.split("0+");
+        System.out.println(Arrays.toString(arr));
+        
+        
+        for(int i=0; i<arr.length; i++) {
+            int count = 0;
+            Long num = Long.parseLong(arr[i]);
+            for(int j=2; j<=(int)Math.sqrt(num); j++) {
+                if(num % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if(num >=2 && count == 0) answer++;
+        }
+        
+        return answer;
+    }
+}
