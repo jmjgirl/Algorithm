64708fa94ed2ab53d1c9e2a682cd3144ca7913a2
Minji Chung <jmjcap123@gmail.com>
2023-11-22 15:25:20 +0900

    [level 2] Title: 타겟 넘버, Time: 0.45 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -6,11 +6,13 @@ class Solution {
     }
     
     public void dfs(int[] numbers, int dept, int target, int sum) {
-        if (dept == numbers.length) { // 마지막 노드까지 탐색한 경우
-            if(sum == target) answer++;
+        if(dept == numbers.length) {
+            if(target == sum) answer++;
         } else {
-            dfs(numbers, dept+1, target, sum + numbers[dept]); // 해당 노드의 값을 더하고 다음 깊이 탐색
+            dfs(numbers, dept+1, target, sum + numbers[dept]);
             dfs(numbers, dept+1, target, sum - numbers[dept]);
-        }       
+        }
     }
+    
+    
 }
