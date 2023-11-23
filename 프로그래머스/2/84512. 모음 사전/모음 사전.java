ec1bc7fc747f0fdd57e9b639ed71308587c940e3
Minji Chung <jmjcap123@gmail.com>
2023-11-23 13:46:00 +0900

    [level 2] Title: 모음 사전, Time: 4.31 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -7,14 +7,7 @@ class Solution {
         int answer = 0;
         list = new ArrayList<>();
         dfs("",0);
-        int size = list.size();
-        for(int i=0; i<list.size(); i++) {
-            if (list.get(i).equals(word)) {
-                answer = i;
-                break;
-            }
-        }
-        return answer;
+        return list.indexOf(word);
     }
     
     public void dfs(String str, int len) {
