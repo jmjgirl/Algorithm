4f12d1d23f681c62342697217c938022256d314c
Minji Chung <jmjcap123@gmail.com>
2023-12-05 12:59:10 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 73.56 ms, Memory: 192 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,37 +1,21 @@
 import java.util.*;
-class Solution {
+class Solution {  //스택을 사용해서 풀어야하는 문제
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
         Stack<Integer> stack = new Stack<>();
-
-        stack.push(0);
-
-        for (int i = 1; i < numbers.length; i++) {
-            // 스택이 비어있지 않으면서 현재 스택이 바라보고 있는 값보다 number의 값이 크면 뒤에 있는 큰 수 해당
-            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+        
+        stack.push(0); //첫번째 인덱스 값
+        for(int i=1; i<numbers.length; i++) {
+            while(!stack.empty() && numbers[stack.peek()] < numbers[i]) {
                 answer[stack.pop()] = numbers[i];
             }
-            stack.push(i);
+            stack.push(i); //다음 인덱스 값을 넣어줌
         }
         
-        // 모든 index를 탐색 후 뒤에 있는 큰 수가 없는 경우 -1 
-        while (!stack.isEmpty()) {
+        // stack에 남은 인덱스 값들 = -1
+        while(!stack.empty()) {
             answer[stack.pop()] = -1;
         }
-
-        /*
-        int index = 0;
-        while(index < numbers.length) {
-            for(int i=index; i<numbers.length; i++) {
-                if(numbers[index] < numbers[i]) {
-                    answer[index] = numbers[i];
-                    break;
-                }
-            }
-            if(answer[index] == 0) answer[index] = -1;
-            index++;
-        }
-        */
         
         return answer;
     }
