4e40e2e5ef5d8058f5f71c1917878ee864516ca9
Minji Chung <jmjcap123@gmail.com>
2023-12-07 10:33:24 +0900

    [level 2] Title: 롤케이크 자르기, Time: 176.35 ms, Memory: 168 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] topping) {
+        int answer = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<topping.length; i++) {
+            map.put(topping[i],map.getOrDefault(topping[i],0)+1);
+        }
+        
+        for(int i=0; i<topping.length; i++) {
+            set.add(topping[i]);
+            map.put(topping[i],map.getOrDefault(topping[i],0)-1);
+            if(map.get(topping[i]) == 0) {
+                map.remove(topping[i]);
+            }
+            
+            if(set.size() == map.size()) answer++;
+        }
+        return answer;
+    }
+}
