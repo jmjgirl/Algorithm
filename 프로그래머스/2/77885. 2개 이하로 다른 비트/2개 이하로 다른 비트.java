8984d0a0c73f8b4b89fc4dc6f0f0fb416f7139a9
Minji Chung <jmjcap123@gmail.com>
2023-12-26 15:54:19 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 86.91 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,18 +1,22 @@
 class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
+        
         for(int i=0; i<numbers.length; i++) {
+            // 1
             if(numbers[i] % 4 != 3) answer[i] = numbers[i] + 1;
-            else {
+            else { // 2
                 String binary = Long.toBinaryString(numbers[i]);
                 int idx = binary.lastIndexOf("0");
-                if(idx != -1) { // 0이 있으면
+                // 2-1 
+                if(idx != -1) {
                     String tmp = binary.substring(0,idx) + "10" + binary.substring(idx+2, binary.length());
                     answer[i] = Long.parseLong(tmp,2);
-                } else {
-                    String tmp = "10" + binary.substring(1, binary.length());
+                } else { // 2-2. 모두 1로만 이루어질때
+                    String tmp = "10" + binary.substring(1,binary.length());
                     answer[i] = Long.parseLong(tmp,2);
                 }
+                
             }
         }
         return answer;
