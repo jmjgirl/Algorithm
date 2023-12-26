6f6adff6895530fbfe6ea33fb28e96a2163d7099
Minji Chung <jmjcap123@gmail.com>
2023-12-26 15:27:48 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 11.12 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -0,0 +1,28 @@
+class Solution {
+    static int[] answer;
+    public int[] solution(int[][] arr) {
+        answer = new int[2];
+        quad(arr,0,0,arr.length);
+        return answer;
+    }
+    
+    public void quad(int[][] arr, int x, int y, int size) {
+        if(zip(arr,x,y,size,arr[x][y])) {
+            if(arr[x][y] == 0) answer[0]++;
+            else answer[1]++;
+            return ;
+        } 
+        quad(arr,x,y,size/2);
+        quad(arr,x+size/2,y,size/2);
+        quad(arr,x,y+size/2,size/2);
+        quad(arr,x+size/2,y+size/2,size/2);
+    }
+    
+    public boolean zip(int[][] arr, int x, int y, int size, int val) {
+        for(int i=x; i<x+size; i++) 
+            for(int j=y; j<y+size; j++) 
+                if(arr[i][j] != val) 
+                    return false;
+        return true;
+    }
+}
