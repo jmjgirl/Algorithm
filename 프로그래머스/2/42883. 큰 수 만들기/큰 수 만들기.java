6bca4a4351c4449e58915604874460e9e8c57cd8
Minji Chung <jmjcap123@gmail.com>
2023-12-29 14:00:05 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.06 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,27 +1,35 @@
 class Solution {
     public String solution(String number, int k) {
-        // 각 자리에서 최고로 높은 수가 나와야함
+        // 그리디 알고리즘
+        // 각 자리에서 최고로 높은 수가 나오는 경우를 생각하기
+        
         String answer = "";
-        StringBuilder sb = new StringBuilder();
+        StringBuilder answerBuilder = new StringBuilder();
+
         
         char[] array = number.toCharArray();
+        
         int len = array.length - k;
         
-        // 문자 비교를 시작하는 인덱스를 나타냄
-        int index = 0;
-        for(int i=0; i<len; i++) {
+        // 문자 비교를 시작하는 인덱스를 나타내는 start 변수 
+        int start = 0;
+
+        for(int i =0; i<len; i++){
             char max = '0';
-            for(int j=index; j<=i+k; j++) {
-                // 가장 큰 수를 골라 다음 인덱스
-                if(array[j] > max) {
+            for(int j = start; j <= i + k; j++){
+                // 가장 큰수를 골라서 그 다음 인덱스를 시작 인덱스로 설정하기 
+                if(array[j] > max){
                     max = array[j];
-                    index = j+1;
+                    start=j+1;
                 }
             }
-            sb.append(max);
+            // 가장 큰 문자를 String에 넣어주기
+            answerBuilder.append(Character.toString(max));
         }
         
-        answer = new String(sb);
+        // k개의 수를 제거할 때 얻을 수 있는 가장 큰 숫자를 구하려 한다 
+        answer = answerBuilder.toString();
         return answer;
     }
+    
 }
