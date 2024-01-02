f9e79c60f11f2eb05effa42ffdfb3cbe2e7901a2
Minji Chung <jmjcap123@gmail.com>
2024-01-02 12:49:29 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 45.04 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -21,7 +21,7 @@ class Solution {
         }
         
         
-        int count = 0;
+        // 각 course에 담긴 갯수별로 최대 주문 수 찾기
         for(int i=0; i<course.length; i++) {
             int max = 0;
             for(String key : map.keySet()) {
@@ -30,34 +30,33 @@ class Solution {
                     max = num;
                 }
             }
-            if(max >= 2) {
-                list[i] = max;
-            }
+            if(max >= 2) list[i] = max;
         }
 
+        // 최대 주문 수를 이용해 key 값 찾아 arr에 저장
         for(int i=0; i<course.length; i++) {
             for(String key : map.keySet()) {
                 int num = map.get(key);
                 if(key.length() == course[i] && num == list[i]) {
                     arr.add(key);
-                    count++;
-
                 }
             }
         }
 
+        // 정렬 한 후 answer 배열에 담기
         Collections.sort(arr);
         String[] answer = new String[arr.size()];
         for(int i=0; i<arr.size(); i++) {
             answer[i] = arr.get(i);
         }
 
-        
         return answer;
     }
     
+    
     public void dfs(String order, String str, int dept, int index) {
         if(dept == order.length()) return ;
+        
         for(int i=index; i<order.length(); i++) {
             if(!visited[i]) {
                 visited[i] = true;
