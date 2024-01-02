7f77abbd23448b1cf2240dcd5cd88f5cac551196
Minji Chung <jmjcap123@gmail.com>
2024-01-02 12:41:54 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 40.92 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -7,20 +7,20 @@ class Solution {
         ArrayList<String> arr = new ArrayList<>();
         int[] list = new int[course.length];
         
-        // 1. 각 Order 정렬
-        for (int i = 0; i < orders.length; i++) {
+        // Orders에 있는 요소들을 오름차순 정렬
+        for (int i=0; i<orders.length; i++) {
             char[] c = orders[i].toCharArray();
             Arrays.sort(c);
             orders[i] = String.valueOf(c);
         }
         
-        // DFS
+        // DFS를 사용하여 조합들을 map에 저장
         for(int i=0; i<orders.length; i++) {
             visited = new boolean[orders[i].length()];
             dfs(orders[i], "", 0, 0);
         }
         
-
+        
         int count = 0;
         for(int i=0; i<course.length; i++) {
             int max = 0;
@@ -47,7 +47,7 @@ class Solution {
         }
 
         Collections.sort(arr);
-        String[] answer = new String[count];
+        String[] answer = new String[arr.size()];
         for(int i=0; i<arr.size(); i++) {
             answer[i] = arr.get(i);
         }
