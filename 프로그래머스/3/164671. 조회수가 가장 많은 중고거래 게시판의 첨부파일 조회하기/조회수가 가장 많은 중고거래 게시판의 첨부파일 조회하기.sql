a9a7207ef5fbac5e46e61cde0dab048cdd3e627f
Minji Chung <jmjcap123@gmail.com>
2024-01-08 10:18:34 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,8 @@
+-- 조회수가 가장 높은 중고거래 게시물 첨부파일 경로 (FILE ID DESC)
+SELECT CONCAT('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_FILE
+WHERE BOARD_ID IN (SELECT BOARD_ID
+                  FROM USED_GOODS_BOARD
+                  WHERE VIEWS = (SELECT MAX(VIEWS)
+                                FROM USED_GOODS_BOARD))
+ORDER BY FILE_ID DESC;
