16e5ada0058d4f87c93d4ac5126ae640efaef1bf
Minji Chung <jmjcap123@gmail.com>
2024-01-17 11:02:24 +0900

    [level 2] Title: 디펜스 게임, Time: 1.07 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -8,13 +8,17 @@ class Solution {
         
         PriorityQueue<Integer> queue = new PriorityQueue<Integer>(Collections.reverseOrder());
         for(int i=0; i<enemy.length; i++) {
-            if(n < enemy[i] && k <= 0) break;
+            // 무적권을 다 썼는데 n이 enemy[i]보다 작아서 막지 못하면 break
+            if(n < enemy[i] && k == 0) break;
 
+            // 우선순위큐에 저장
             queue.add(enemy[i]);
+            // n이 enemy[i] 보다 작으면 우선순위큐에서 가장 많은 적의 수를 꺼내고 무적권 사용
             if(n < enemy[i]) { 
                 n += queue.poll();
                 k--;
             }
+            
             n -= enemy[i];
             answer++;
         }
