67c065e9efea29935dbb8949acd825a6f41265be
Minji Chung <jmjcap123@gmail.com>
2024-02-28 12:20:27 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        //1 2 입력받기
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+
+        //A+B 출력하기
+        System.out.println(A+B);
+    }
+}
