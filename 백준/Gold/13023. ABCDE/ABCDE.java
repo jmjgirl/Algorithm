7017be8eb8f6162f1d29b108b3213d1caa8788e5
Minji Chung <jmjcap123@gmail.com>
2024-03-20 18:42:12 +0900

    [Gold V] Title: ABCDE, Time: 524 ms, Memory: 40216 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main {
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int N;
+    static int count;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        N = scan.nextInt();
+        int M = scan.nextInt();
+        graph = new ArrayList[N];
+        visited = new boolean[N];
+
+        // 그래프 초기화
+        for(int i=0; i<N; i++) {
+            graph[i] = new ArrayList<Integer>();
+        }
+
+        for(int i=0; i<M; i++) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        count = 0;
+        for(int i=0; i<N; i++) {
+            dfs(i,1); //dept : 1부터 시작
+        }
+        System.out.println(count);
+    }
+
+    public static void dfs(int v, int dept) {
+        if(dept==5 || count == 1) {
+            count = 1;
+            return;
+        }
+
+        visited[v] = true;
+        for(int next : graph[v]) {
+            if(!visited[next]) {
+                dfs(next,dept+1);
+            }
+        }
+        visited[v] = false;
+    }
+}
